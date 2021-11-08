5e77c8925d3f0bde23d30bbba9032bf3d36e997d
lmn165 <lastalking@naver.com>
2021-11-08 11:56:28 +0900

    Nov 08 update LocalMap

--- frontend/src/features/map/components/LocalMap.js
+++ frontend/src/features/map/components/LocalMap.js
@@ -1,37 +1,79 @@
-import React from "react";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import { PropTypes } from "prop-types";
+import React, { useEffect } from "react";
+const { kakao } = window;
 
-const LocalMap = props => {
-    const mapStyles = {
-        width: "100%",
-        height: "100%"
-      };
-    return(<>
-        <h1>Local Map 페이지</h1>
-        <Map
-            google={props.google}
-            zoom={10}
-            style={mapStyles}
-            initialCenter={{ lat: props.latitude, lng: props.longitude }}
-        >
-            <Marker
-            position={{ lat: props.latitude, lng: props.longitude }}
-            icon={{
-                url: `${process.env.PUBLIC_URL + "/assets/img/icon-img/2.png"}`
-            }}
-            animation={props.google.maps.Animation.BOUNCE}
-            />
-        </Map>
-    </>)
+
+function LocalMap() {
+  useEffect(() => {
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    
+    // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
+    var positions = [
+        {
+            content: '<div>카카오</div>', 
+            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+        },
+        {
+            content: '<div>생태연못</div>', 
+            latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+        },
+        {
+            content: '<div>텃밭</div>', 
+            latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+        },
+        {
+            content: '<div>근린공원</div>',
+            latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+        }
+    ];
+
+    for (var i = 0; i < positions.length; i ++) {
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng // 마커의 위치
+        });
+
+        // 마커에 표시할 인포윈도우를 생성합니다 
+        var infowindow = new kakao.maps.InfoWindow({
+            content: positions[i].content // 인포윈도우에 표시할 내용
+        });
+
+        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+        // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+        kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+    }
+
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+    function makeOverListener(map, marker, infowindow) {
+        return function() {
+            infowindow.open(map, marker);
+        };
+    }
+
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+    function makeOutListener(infowindow) {
+        return function() {
+            infowindow.close();
+        };
+    }
+  }, []);
+
+  return (
+    <div
+      className="map"
+      id="map"
+      style={{ width: "1000px", height: "500px" }}
+    //   ref={mapContainer}
+    ></div>
+  );
 }
 
-LocalMap.propTypes = {
-    google: PropTypes.object,
-    latitude: PropTypes.string,
-    longitude: PropTypes.string
-  };
-
-  export default GoogleApiWrapper({
-    apiKey: "AIzaSyB2D8wrWMY3XZnuHO6C31uq90JiuaFzGws"
-  })(LocalMap)
+export default LocalMap;
--- frontend/src/features/map/pages/LocalMapPage.js
+++ frontend/src/features/map/pages/LocalMapPage.js
@@ -1,24 +1,11 @@
-import PropTypes from "prop-types";
-import React, { Fragment } from "react";
+import React from "react";
 import { LocalMap } from "..";
 
 const LocalMapPage = () => {
 
   return (
-    //   <LayoutOne>
-              <LocalMap/>
-    //   </LayoutOne>
+    <LocalMap/>
   );
 };
 
 export default LocalMapPage;
-
-
-// import React from "react";
-// import { LocalMap } from "..";
-// 
-// export default function Local() {
-    // return(<>
-        {/* <LocalMap/> */}
-    {/* </>) */}
-// }
