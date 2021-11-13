345c63595a2f56a8f30a4be392c4ff8b96675a2a
unknown <lmn1654@gmail.com>
2021-11-13 18:02:45 +0900

    Nov 13 update chatbot

--- backend/map/modes_data.py
+++ backend/map/modes_data.py
@@ -4,50 +4,33 @@ import csv
 import sys
 from common.models import ValueObject, Printer, Reader
 # system setup
-from product.models import Vendor, Category, Product
+from map.models import Map
 # SET FOREIGN_KEY_CHECKS = 0;
 class DbUploader():
     def __init__(self):
         vo = ValueObject()
         reader = Reader()
         self.printer = Printer()
-        vo.context = 'product/data/'
-        vo.fname='product.csv'
+        vo.context = 'map/data/'
+        vo.fname='med_point_211111.csv'
         self.csvfile = reader.new_file(vo)
 
     def insert_data(self):
-        self.insert_vendor()
-        self.insert_category()
-        self.insert_product()
+        self.insert_map()
 
-    def insert_vendor(self):
-        with open(self.csvfile, newline='', encoding='utf8') as f:
-            data_reader = csv.DictReader(f)
-            for row in data_reader:
-                if not Vendor.objects.filter(name=row['vendor']).exists():
-                    vendor = Vendor.objects.create(name=row['vendor'])
-        print('VENDOR DATA UPLOADED SUCCESSFULY!')
-
-    def insert_category(self):
-        with open(self.csvfile, newline='', encoding='utf8') as f:
-            data_reader = csv.DictReader(f)
-            for row in data_reader:
-                if not Category.objects.filter(name=row['category']).exists():
-                    category = Category.objects.create(name=row['category'])
-        print('CATEGORY DATA UPLOADED SUCCESSFULY!')
-
-    def insert_product(self):
+    def insert_map(self):
         with open(self.csvfile, newline='', encoding='utf8') as csvfile:
             data_reader = csv.DictReader(csvfile)
             for row in data_reader:
-                v = Vendor()
-                vendor = Vendor.objects.all().filter(name=row['vendor']).values()[0]
-                v.id = vendor['id']
-                c = Category()
-                category = Category.objects.all().filter(name=row['category']).values()[0]
-                c.id = category['id']
-                Product.objects.create(name=row['product'] ,
-                                       price=row['price'],
-                                       category=c,
-                                       vendor=v)
+                m = Map()
+                map = Map.objects.all().filter(name=row['name']).values()[0]
+                m.id = map['id']
+                Map.objects.create(type=row['product'] ,
+                                       name=row['price'],
+                                       address=row['price'],
+                                       lat=row['price'],
+                                       long=row['price'],
+                                       total=row['price'],
+                                       infected=row['price'],
+                                       med_point_id=m)
             print('PRODUCT DATA UPLOADED SUCCESSFULY!')
