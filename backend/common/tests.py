c59e60568b8020da8b2821a32bb44d24a6d0a0b6
lmn165 <lastalking@naver.com>
2021-11-15 11:26:46 +0900

    Nov 15 update testcode

--- backend/common/tests.py
+++ backend/common/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 # https://www.tensorflow.org/guide/gpu
 import tensorflow as tf
+from tensorflow.python.client import device_lib
+
 
 if __name__ == '__main__':
     tf.debugging.set_log_device_placement(True)
@@ -9,4 +11,5 @@ if __name__ == '__main__':
     b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
     c = tf.matmul(a, b)
     print(c)
+    # print(device_lib.list_local_devices())
 
