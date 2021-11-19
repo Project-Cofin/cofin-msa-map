ed7e00648fccdf70dfa0cb27d2fab1cf6acede4e
lmn165 <lastalking@naver.com>
2021-11-19 17:37:48 +0900

    Nov 19 update cases points

--- frontend/src/features/map/components/Geolocation.js
+++ frontend/src/features/map/components/Geolocation.js
@@ -8,8 +8,8 @@ const Geolocation = () => {
         if (navigator.geolocation) { // GPS를 지원하면
             navigator.geolocation.getCurrentPosition(function(position) {
             //   alert(position.coords.latitude + ' ' + position.coords.longitude);
-            // dispatch(medPoints({'latitude': position.coords.latitude, 'longitude': position.coords.longitude}))
-            dispatch(casesPoints({'latitude': position.coords.latitude, 'longitude': position.coords.longitude}))
+            dispatch(medPoints({'latitude': position.coords.latitude, 'longitude': position.coords.longitude}))
+            // dispatch(casesPoints({'latitude': position.coords.latitude, 'longitude': position.coords.longitude}))
             window.localStorage.setItem('sessionGeo', [position.coords.latitude, position.coords.longitude])
             }, function(error) {
               console.error(error);
@@ -20,6 +20,7 @@ const Geolocation = () => {
             });
           } else {
             alert('GPS를 지원하지 않습니다');
+            window.localStorage.setItem('sessionGeo', ['37.5885933100535', '127.101500688631'])
           }
     },[])
     return(<></>)
