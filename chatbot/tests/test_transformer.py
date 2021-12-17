4ad1cf4ec59b613d59ce73b2b5461b6f1370d925
lmn165 <lastalking@naver.com>
2021-12-17 17:15:35 +0900

    Dec 17 add test sample

--- chatbot/tests/test_transformer.py
+++ chatbot/tests/test_transformer.py
@@ -104,67 +104,67 @@ class TransformerChat:
         print(f'처음과 마지막 패딩 토큰을 제거한 샘플: {intents[58:59][:, 1:-1]}')    # 길이는 8이 된다.
 
         # ------------- 트랜스포머 생성 -------------
-        tf.keras.backend.clear_session()
-        model = transformer(
-            vocab_size=VOCAB_SIZE,
-            num_layers=self.NUM_LAYERS,
-            dff=self.DFF,
-            d_model=self.D_MODEL,
-            num_heads=self.NUM_HEADS,
-            dropout=self.DROPOUT)
-        learning_rate = 0.001
+        # tf.keras.backend.clear_session()
+        # model = transformer(
+        #     vocab_size=VOCAB_SIZE,
+        #     num_layers=self.NUM_LAYERS,
+        #     dff=self.DFF,
+        #     d_model=self.D_MODEL,
+        #     num_heads=self.NUM_HEADS,
+        #     dropout=self.DROPOUT)
+        # learning_rate = 0.001
+        #
+        # optimizer = tf.keras.optimizers.Adam(
+        #     learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+        #
+        # def accuracy(y_true, y_pred):
+        #     # 레이블의 크기는 (batch_size, MAX_LENGTH - 1)
+        #     y_true = tf.reshape(y_true, shape=(-1, self.INTENT_MAX_LENGTH - 1))
+        #     return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+        #
+        # model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=[accuracy])
+        # model.fit(dataset, epochs=self.EPOCHS)
 
-        optimizer = tf.keras.optimizers.Adam(
-            learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
-
-        def accuracy(y_true, y_pred):
-            # 레이블의 크기는 (batch_size, MAX_LENGTH - 1)
-            y_true = tf.reshape(y_true, shape=(-1, self.INTENT_MAX_LENGTH - 1))
-            return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
-
-        model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=[accuracy])
-        model.fit(dataset, epochs=self.EPOCHS)
-
-
-def transformer(vocab_size, num_layers, dff,
-                d_model, num_heads, dropout,
-                name="transformer"):
-
-      # 인코더의 입력
-      inputs = tf.keras.Input(shape=(None,), name="inputs")
-
-      # 디코더의 입력
-      dec_inputs = tf.keras.Input(shape=(None,), name="dec_inputs")
-
-      # 인코더의 패딩 마스크
-      enc_padding_mask = tf.keras.layers.Lambda(
-          create_padding_mask, output_shape=(1, 1, None),
-          name='enc_padding_mask')(inputs)
-
-      # 디코더의 룩어헤드 마스크(첫번째 서브층)
-      look_ahead_mask = tf.keras.layers.Lambda(
-          create_look_ahead_mask, output_shape=(1, None, None),
-          name='look_ahead_mask')(dec_inputs)
-
-      # 디코더의 패딩 마스크(두번째 서브층)
-      dec_padding_mask = tf.keras.layers.Lambda(
-          create_padding_mask, output_shape=(1, 1, None),
-          name='dec_padding_mask')(inputs)
-
-      # 인코더의 출력은 enc_outputs. 디코더로 전달된다.
-      enc_outputs = encoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff,
-          d_model=d_model, num_heads=num_heads, dropout=dropout,
-      )(inputs=[inputs, enc_padding_mask]) # 인코더의 입력은 입력 문장과 패딩 마스크
-
-      # 디코더의 출력은 dec_outputs. 출력층으로 전달된다.
-      dec_outputs = decoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff,
-          d_model=d_model, num_heads=num_heads, dropout=dropout,
-      )(inputs=[dec_inputs, enc_outputs, look_ahead_mask, dec_padding_mask])
-
-      # 다음 단어 예측을 위한 출력층
-      outputs = tf.keras.layers.Dense(units=vocab_size, name="outputs")(dec_outputs)
-
-      return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
+#
+# def transformer(vocab_size, num_layers, dff,
+#                 d_model, num_heads, dropout,
+#                 name="transformer"):
+#
+#       # 인코더의 입력
+#       inputs = tf.keras.Input(shape=(None,), name="inputs")
+#
+#       # 디코더의 입력
+#       dec_inputs = tf.keras.Input(shape=(None,), name="dec_inputs")
+#
+#       # 인코더의 패딩 마스크
+#       enc_padding_mask = tf.keras.layers.Lambda(
+#           create_padding_mask, output_shape=(1, 1, None),
+#           name='enc_padding_mask')(inputs)
+#
+#       # 디코더의 룩어헤드 마스크(첫번째 서브층)
+#       look_ahead_mask = tf.keras.layers.Lambda(
+#           create_look_ahead_mask, output_shape=(1, None, None),
+#           name='look_ahead_mask')(dec_inputs)
+#
+#       # 디코더의 패딩 마스크(두번째 서브층)
+#       dec_padding_mask = tf.keras.layers.Lambda(
+#           create_padding_mask, output_shape=(1, 1, None),
+#           name='dec_padding_mask')(inputs)
+#
+#       # 인코더의 출력은 enc_outputs. 디코더로 전달된다.
+#       enc_outputs = encoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff,
+#           d_model=d_model, num_heads=num_heads, dropout=dropout,
+#       )(inputs=[inputs, enc_padding_mask]) # 인코더의 입력은 입력 문장과 패딩 마스크
+#
+#       # 디코더의 출력은 dec_outputs. 출력층으로 전달된다.
+#       dec_outputs = decoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff,
+#           d_model=d_model, num_heads=num_heads, dropout=dropout,
+#       )(inputs=[dec_inputs, enc_outputs, look_ahead_mask, dec_padding_mask])
+#
+#       # 다음 단어 예측을 위한 출력층
+#       outputs = tf.keras.layers.Dense(units=vocab_size, name="outputs")(dec_outputs)
+#
+#       return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
 
 
 
